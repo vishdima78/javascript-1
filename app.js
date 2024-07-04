103e2a07c915826520178e02aad8eea8c496cbaa
vishdima <dmitriivishnyakov2003@gmail.com>
2024-07-05 00:37:39 +0400

    Василий Пупкин Практика теории

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log ('Hello')
+const projectName = 'Сайт магазина';
+const price = 2000;
+const author = 'Василий Пупкин';
+
+
+const template = author + ' заказал ' + projectName + ' по цене ' + price + '$';
+console.log(template);
+const template2 = `${author} заказал ${projectName} по цене ${price}$`;
+
+
+console.log(template2);
+
+const template3 = 'проект \n' + 'цена: ' + price + '$';
+console.log(template3);
+const template4 = `Проект
+Цена: ${price}$`;
+console.log(template4);
