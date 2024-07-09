d6f3e6cdb318e592e8d105d0ad7d4a979f34b071
vishdima <dmitriivishnyakov2003@gmail.com>
2024-07-09 14:53:06 +0400

    Функции высшего порядка

--- app.js
+++ app.js
@@ -1,160 +1,40 @@
-// for(let i = 1; i < 10; i++) {
-// console.log(`Наш баланс ${i}$`);
+// function plus(a, b) {
+//     return a + b;
+// }
+// function minus(a, b) {
+//     return a - b;
+// }
+// function multiplication(a, b) {
+//     return a * b;
+// }
+// function division(a, b) {
+//     return a / b;
 // }
 
 
-
-// const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
-
-// for (let i = 0; i < tasks.length; i++) {
-//     if (tasks[i] === 'Задача 2') {
-//         break;
-//     }
-//     console.log(tasks[i]);
+// function multi(a, b, function_name) {
+//     console.log(function_name.name);
+//     const res = function_name(a, b);
+//     return res;
 // }
 
+// let res = multi(1, 2, division);
+// console.log(res);
 
-// const arr = ['!', 'JS', 'люблю', 'Я'];
-// const result = [];
-
-// for (let i = arr.length-1; i >= 0; i--) {
-//     result.push(arr[i]);
-    
-// }
-// console.log(result.join(' '));
-
-
-
-// const tasks = [[1, 'Задача 1'], [2, 'Задача 2']]
-
-// for (let i = 0; i < tasks.length; i++) {
-//     for (let j = 0; j < tasks[i].length; j++) {
-//         console.log(tasks[i][j]);
+// function power(pow) {
+//     return function(num) {
+//         return num**pow;
 //     }
 // }
 
+// const powerOfTwo = power(2);
+// console.log(powerOfTwo(5));
 
-// for (let i = 1; i < 5; i++) {
-//     console.log(`Вывод - ${i}`);
-// }
+// const powerOfThree = power(3);
+// console.log(powerOfThree(5));
 
-// let i = 1;
-// while (i < 5) {
-//     console.log(`Вывод - ${i}`);
-//     i++;
-// }
+// power(5)(4)
+// console.log(power(5)(4));
 
-// const arr = [1, 4, 8, 7];
-
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > 5) {
-//         break;
-//     }
-//     console.log(arr[i]);
-// }
-
-// let i = 0
-// while (arr[i] <= 5 && i < arr.length) {
-//     console.log(arr[i]);
-//     i++;
-// }
-// let j = 0;
-// do {
-//     console.log(j);
-//     j++;
-// } while (j < 0)
-
-
-
-// const arr = [1, 4, 8, 7];
-
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-
-// for (let element of arr) {
-//     console.log(element);
-// }
-
-// for (let index in arr) {
-//      console.log(arr[index]);
-// }
-
-
-
-
-// const operation = [5, 4, 3, 2];
-// let startBalance = 100;
-// let avgIncome = 0;
-// let avgConsumption = 0;
-
-// function closingBalance () {
-//     for (let element of operation) {
-//         startBalance += element;
-//         if (startBalance < 0) {
-//             return console.log(false);
-//         }
-//     }
-
-//     console.log(startBalance);
-// }
-
-// closingBalance();
-
-// function avgBalance () {
-//     for (let element of operation) {
-//         if (element <= 0) {
-        
-//         avgConsumption += element;
-//         } else {
-//         avgIncome += element;
-//         }
-
-//     }
-//     console.log(`Средний доход ${avgIncome} ; Средний расход ${avgConsumption}`)
-//     }
-// avgBalance ();
-
-
-const operation = [5, 224, -30, -2];
-const startingBalance = 100;
-function getBalance(arrayOfOperations, initailBalance) {
-    let balance = initailBalance;
-    for (const element of arrayOfOperations) {
-        balance += element;
-    }
-    return balance;
-}
-console.log(getBalance(operation, startingBalance));
-
-function checkOperations(arrayOfOperations, initailBalance) {
-     let balance = initailBalance;
-     let isOk = true
-    for (const element of arrayOfOperations) {
-        balance += element;
-        if (balance < 0) {
-            isOk = false;
-            break;
-        }
-    }
-    return isOk;
-}
-
-function avarageOperations (arrayOfOperations) {
-    let positiveCount = 0;
-    let positiveSum = 0;
-    let negativeCount = 0;
-    let negativeSum = 0;
-    for (const element of arrayOfOperations) {
-        if (element > 0) {
-            positiveCount++;
-            positiveSum += element
-        }
-        if (element < 0) {
-            negativeCount++;
-            negativeSum += element;
-        }
-    }
-    return[positiveSum / positiveCount, negativeSum / negativeCount];
-}
-console.log(avarageOperations(operation))
+// const power = pow => num => num**pow;
+// console.log(power(2)(4));
